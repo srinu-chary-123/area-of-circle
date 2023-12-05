b57d56a098707bbebe57ec8932ad717651829e18
chary <132874723+srinu-chary-123@users.noreply.github.com>
2023-12-05 22:44:20 +0530

    Add files via upload

---
+++ Area of circle/area.js
@@ -0,0 +1,8 @@
+function AREAOFCIRCLE(){
+    var Enterradiusvalue=eval(document.getElementById('id1').value)
+    var result=3.14*Enterradiusvalue**2
+    alert(result)  
+}
+ function EASER(){
+    document.getElementById('id1').value=""
+ }
